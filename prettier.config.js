521454f86f68c3c44cf02c6313e7dee13e69499e
Mateusz Nowak <mateusz.nowak@put.poznan.pl>
2022-06-11 14:17:48 +0200

    Initial project.

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 185,
+  trailingComma: "none",
+  semi: false,
+  tabWidth: 2,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  proseWrap: "never",
+  vueIndentScriptAndStyle: true,
+  arrowParens: "avoid",
+  htmlWhitespaceSensitivity: "strict"
+}
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,8 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+  import {onMounted} from "vue"
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  onMounted(() => {})
 </script>
+<template><div></div></template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped lang="scss"></style>

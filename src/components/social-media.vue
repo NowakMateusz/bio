c708b898b56fd26c902759a47423439596f6f504
mateusz.nowak@put.poznan.pl <mateusz.nowak@put.poznan.pl>
2022-06-13 14:12:56 +0200

    Fix for Firefox compatibility.

--- src/components/social-media.vue
+++ src/components/social-media.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="social-media">
-    <a target="_blank" href="https://github.com/NowakMateusz/bio/"><img alt="GitHub link" src="@/assets/github.png" /></a>
-    <a target="_blank" href="https://www.linkedin.com/in/mateusz-nowak-4578aaaa"><img alt="LinkedIn link" src="@/assets/linkedin.png" /></a>
-    <a onclick="window.print()"><img alt="Print CV" src="@/assets/printer.png" /></a>
+    <div id="social">
+      <a target="_blank" href="https://github.com/NowakMateusz/bio/"><img alt="GitHub link" src="@/assets/github.png" /></a>
+      <a target="_blank" href="https://www.linkedin.com/in/mateusz-nowak-4578aaaa"><img alt="LinkedIn link" src="@/assets/linkedin.png" /></a>
+      <a onclick="window.print()"><img alt="Print CV" src="@/assets/printer.png" /></a>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
   #social-media {
     position: sticky;
-    display: table-row;
     top: 20px;
     left: 20px;
 
-    a {
-      width: auto;
-      cursor: pointer;
+    #social {
+      display: flex;
+      flex-direction: column;
+      a {
+        width: auto;
+        cursor: pointer;
 
-      img {
-        display: table-cell;
-        padding: 10px 15px;
-        height: 4rem;
+        img {
+          padding: 10px 15px;
+          height: 4rem;
+        }
       }
     }
   }
